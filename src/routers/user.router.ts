ee36f6feeb209ab8522f1af64c2f9c0a89160567
Ananta <anantawijaya920@gmail.com>
2025-04-07 15:06:50 +0700

    update routers

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -15,11 +15,29 @@ export class UserRouter {
     this.initializeRouter();
   }
   private initializeRouter() {
-    this.router.post("/",  this.usersController.createUser);
-    this.router.get("/", this.authMiddleware.verifyToken, this.usersController.getUsers);
-    this.router.get("/posts", this.authMiddleware.verifyToken, this.usersController.getUserPosts);
+    this.router.post("/", this.usersController.createUser);
+    this.router.get(
+      "/",
+      this.authMiddleware.verifyToken,
+      this.usersController.getUsers
+    );
+    this.router.get(
+      "/posts",
+      this.authMiddleware.verifyToken,
+      this.usersController.getUserPosts
+    );
     this.router.get("/:id", this.usersController.getUserId);
-    this.router.patch("/edit", this.authMiddleware.verifyToken, this.usersController.updateUser);
+    this.router.patch(
+      "/edit",
+      this.authMiddleware.verifyToken,
+      this.usersController.updateUser
+    );
+    this.router.delete(
+      "/delete/:id",
+      this.authMiddleware.verifyToken,
+      this.authMiddleware.verifyAdmin,
+      this.usersController.delUser
+    );
   }
   getRouter(): Router {
     return this.router;
