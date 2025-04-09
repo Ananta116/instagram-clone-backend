d0b2f0c7a3849e0e922fc187b43f3c77c12d7c5f
Ananta <anantawijaya920@gmail.com>
2025-04-09 11:28:03 +0700

    gmail send & verification

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,10 @@ import prisma from "../prisma";
 import { Request, Response } from "express";
 import { compare, genSalt, hash } from "bcrypt";
 import { sign } from "jsonwebtoken";
+import { transporter } from "../helpers/mailer";
+import path from "path";
+import fs from "fs";
+import handlebars from "handlebars";
 
 export class AuthController {
   async register(req: Request, res: Response) {
@@ -13,6 +17,23 @@ export class AuthController {
 
       const user = await prisma.users.create({
         data: { username, email, password: hashedPass, fullname },
+      });
+
+      const payload = { id: user.id, role: "user" };
+      const token = sign(payload, process.env.KEY_JWT!, { expiresIn: "10m" });
+      const link = `${process.env.URL_FE}/verify/${token}`;
+      // link : http://localhost:3000 (nanti ganti pake link deployment + /verify + /token
+
+      const templatePath = path.join(__dirname, "../templates", `verify.hbs`);
+      const templateSource = fs.readFileSync(templatePath, "utf-8");
+      const compiledTemplate = handlebars.compile(templateSource);
+      const html = compiledTemplate({ username });
+
+      await transporter.sendMail({
+        from: process.env.GMAIL_USER,
+        to: email,
+        subject: "verification Email",
+        html,
       });
       res.status(200).send({
         message: "user registered✅",
@@ -33,13 +54,28 @@ export class AuthController {
       if (!isValidPass) throw { message: "Incorrect password" };
 
       const payload = { id: user.id, role: "user" };
-      const access_token = sign(payload, process.env.KEY_JWT!, { expiresIn: "1h" });
+      const access_token = sign(payload, process.env.KEY_JWT!, {
+        expiresIn: "1h",
+      });
 
       res.status(200).send({
         message: "Login success",
         data: user,
         access_token,
       });
+    } catch (error) {
+      console.log(error);
+      res.status(400).send(error);
+    }
+  }
+  async verify(req: Request, res: Response) {
+    try {
+      await prisma.users.update({
+        data: { isVerify: true },
+        where: { id: req.user?.id },
+      });
+
+      res.status(200).send({ message: "verification success!" });
     } catch (error) {
       console.log(error);
       res.status(400).send(error);
